d1df06c0c2cea2a773044345263b9d478d5bbcad
pascalt <pascaltoutain@gmail.com>
2023-08-08 20:18:30 +0200

    SYNTAX 2

--- app/app/test.py
+++ app/app/test.py
@@ -4,9 +4,9 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         res = calc.add(2, 4)
         self.assertEqual(res, 6)
-        
